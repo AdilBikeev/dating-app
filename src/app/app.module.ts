cd626eeaa4a648bb9cb2a5d2ddff7bddda4e4ccb
AdCoder <bikeev1999@mail.ru>
2021-01-02 22:25:02 +0300

    master First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import ruLocale from '@angular/common/locales/ru';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+
+/**
+ * Добавляем локализацию
+ */
+registerLocaleData(ruLocale, 'ru')
 
 @NgModule({
   declarations: [
